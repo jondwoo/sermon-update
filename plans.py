e65537b1a694a203511283732b17b9b3d393bb58
Jon Ham <23408816+jondwoo@users.noreply.github.com>
2020-05-17 16:31:48 -0700

    initial commit

---
+++ plans.py
@@ -0,0 +1,28 @@
+"""
+Project Details:
+  - list out past 6 sunday plans
+    - sermon title
+    - sermon series
+    - date
+    - preacher
+"""
+import requests, json
+import tokens
+
+url = "https://api.planningcenteronline.com/services/v2/service_types/800507/plans?offset=94"
+r = requests.get(url, auth=(tokens.APP_ID, tokens.SECRET))
+data = json.loads(r.text)
+
+
+def getSundayPlanIDs():
+    id_list = []
+    for i in data["data"]:
+        id_list.append(i["id"])
+        # if i["relationships"]["next_plan"]["data"] != None:
+        #     print(json.dumps(i["relationships"]["next_plan"]["data"]["id"], indent=2))
+    return id_list
+
+
+if __name__ == "__main__":
+    id_list = getSundayPlanIDs()
+    print(id_list)
