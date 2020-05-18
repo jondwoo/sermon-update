128ea7f799498d877ab3110c951dab567b02abb9
Jon Ham <23408816+jondwoo@users.noreply.github.com>
2020-05-18 16:04:17 -0700

    edit comments

--- sermon_info.py
+++ sermon_info.py
@@ -109,7 +109,7 @@ def getNextIDs(curr_id):
     next_id = ''
     next_next_id = ''
 
-    # get next/prev ID
+    # get next
     plan_details_url = (
         f'https://api.planningcenteronline.com/services/v2/service_types/764160/plans/{curr_id}/')
     r = requests.get(
