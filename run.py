bf3923b04cbf0144edd83af90933014a59cc2834
Jon <jondwoo@gmail.com>
2020-05-18 16:00:53 -0700

    import youtube methods

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
-import plans
+import sermon_info
 import youtube
+import json
 from datetime import datetime
 
 if __name__ == "__main__":
+    sermon_list = []
     while(True):
-        curr_id = plans.getCurrPlanID()
-        next_id, next_next_id = plans.getNextIDs(curr_id)
+        sermons = {}
+        # PCO
+        curr_id = sermon_info.getCurrPlanID()
+        next_id, next_next_id = sermon_info.getNextIDs(curr_id)
 
-        sermon_date, next_sermon_date = plans.getPlanDates(curr_id, next_id)
+        sermon_date, next_sermon_date = sermon_info.getPlanDates(curr_id, next_id)
         sermon_date_obj = datetime.strptime(sermon_date, '%B %d, %Y').date()
         next_sermon_date_obj = datetime.strptime(next_sermon_date, '%B %d, %Y').date()
 
-        sermon_title = plans.getPlanTitle(curr_id)
-        scripture = plans.getPlanScripture(curr_id)
-        speaker = plans.getPlanSpeaker(curr_id)
-        series_title = plans.getPlanSeries(curr_id)
+        sermon_title = sermon_info.getPlanTitle(curr_id)
+        scripture = sermon_info.getPlanScripture(curr_id)
+        speaker = sermon_info.getPlanSpeaker(curr_id)
+        series_title = sermon_info.getPlanSeries(curr_id)
 
         print(f'Plan ID: {curr_id}')
         print(f'Date: {sermon_date}')
@@ -23,16 +27,37 @@ if __name__ == "__main__":
         print(f'Speaker: {speaker}')
         print(f'Series: {series_title}')
 
+        sermons['plan_id'] = curr_id
+        sermons['date'] = sermon_date
+        sermons['sermon_title'] = sermon_title
+        sermons['scripture'] = scripture
+        sermons['speaker'] = speaker
+        sermons['series'] = series_title
+        sermons['next_sermon_date'] = next_sermon_date_obj.strftime('%Y-%m-%d')
+        sermons['todays_date'] = datetime.date(datetime.now()).strftime('%Y-%m-%d')
+        sermon_list.append(sermons)
+
         # if current plan's next date <= today, keep looping
-        isNextDate = plans.compareDates(next_sermon_date_obj)
+        isNextDate = sermon_info.compareDates(next_sermon_date_obj)
         if isNextDate:
             pass
         else:
             break
-
-        plans.updateCurrID(next_id, next_next_id, curr_id)
-
         
+        sermon_info.updateCurrID(next_id, next_next_id, curr_id)
+
+    print('SERMON LIST')
+    print(json.dumps(sermon_list,indent=2))
+    print('')
+
+    # YOUTUBE
+    youtube_resource = youtube.authenticateYoutubeAPI()
+    nlpc_resource = youtube.getChannelResource(youtube_resource)
+    videos = youtube.getVideos(youtube_resource, nlpc_resource)
+
+    print('YOUTUBE')
+    print(json.dumps(videos,indent=2))
+
 
 # Jan 5, 2020: 40782442
 # Apr 5, 2020: 46074866
