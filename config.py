0cf9e24e901d52bad9d8e8d2bd7e4c010cb66844
Jon Ham <23408816+jondwoo@users.noreply.github.com>
2020-05-21 16:08:09 -0700

    clean up

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # Number of youtube videos to return latest to oldest
 maxResults=26
 
-row=4
-col=3
+# Number of rows and columns to display on web page
+row=3
+col=5
 
 # Number of sermons to retrieve from database
 limit_val=row*col
